3b61ec3a01416f54e439cbbcbb73ce295a7a7303
BoonSinHaw <n301030@gmail.com>
2025-11-12 03:07:40 +0800

    1

---
+++ js/homebanner.js
@@ -0,0 +1,31 @@
+function start() {
+    var rotator = document.getElementById('rotator');
+    var delayInSeconds = 10;
+    var imageDir = 'images/';
+    var images = ['homebanner1.png', 'homebanner2.jpg', 'homebanner3.webp', 'homebanner4.webp'];
+    var num = 0;
+
+    // Set initial opacity and transition
+    rotator.style.transition = "opacity 1.5s ease-in-out";
+    rotator.style.opacity = 1;
+
+    var changeImage = function () {
+        // Fade out
+        rotator.style.opacity = 0;
+
+        setTimeout(function () {
+            // Change image after fade out completes
+            rotator.src = imageDir + images[num++];
+            if (num == images.length) {
+                num = 0;
+            }
+
+            // Fade in new image
+            rotator.style.opacity = 1;
+        }, 1500); // match fade duration (1.5s)
+    };
+
+    setInterval(changeImage, delayInSeconds * 1000);
+}
+
+window.onload = start;
